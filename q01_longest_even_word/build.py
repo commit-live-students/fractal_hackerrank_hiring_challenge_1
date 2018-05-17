7f13287cb7dc58a2d202b07bf25238dc32a5638e
sonal7930 <sonal.chandra7930@gmail.com>
2018-05-17 10:52:31 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,13 @@
-# Default imports
-
-import re
+import sys
+import nltk
 from nltk.tokenize import word_tokenize
 
-# Write your solution here :
+sentence = sys.argv[1]
+
 def q01_longest_even_word(sentence):
-    
-    
+    word=word_tokenize(sentence)
+    list1 =[word for word in word if len(word) % 2 == 0]
+    if(len(list1) == 0):
+        return 0
+    else:
+        return max(list1, key = len)
