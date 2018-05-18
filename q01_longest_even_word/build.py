13397d1d363796d7472cf419a8e0c66d36f90579
siddesh001 <siddesh.001@gmail.com>
2018-05-18 12:25:08 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,22 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
 from nltk.tokenize import word_tokenize
 
 # Write your solution here :
-def q01_longest_even_word(sentence):
+def q01_longest_even_word(d):
+    d=d.replace(',','')
+    d=d.replace('.','')
+    d=d.split()
     
-    
+    max_len=0
+    maxx=0
+    for word in d:
+        if len(word)%2==0 and maxx<len(word):
+         
+            max_len=word
+            maxx=len(word)
+        
+    return max_len
+
