8af8a2aa0a19ba622c6e7e0b7c9f6893fb71c3f5
ankitalalwani <lalwaniankita20@gmail.com>
2018-06-28 09:16:51 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,3 +1,4 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
@@ -6,4 +7,17 @@ from nltk.tokenize import word_tokenize
 # Write your solution here :
 def q01_longest_even_word(sentence):
     
-    
+    max = 0
+    data = sentence.split()
+    long_word = 0
+    for i in data:
+        if(len(i) % 2 == 0):
+            if(len(i) > max):
+                max = len(i)
+                long_word = i
+    return long_word
+
+q01_longest_even_word(' testing very9 important')
+
+
+
