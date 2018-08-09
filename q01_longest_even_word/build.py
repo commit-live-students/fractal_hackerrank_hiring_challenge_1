958a7971434d7aa246723a570185f1eaa2ea2b53
rushabh-mehta <rushabh123@gmail.com>
2018-08-09 08:50:33 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,17 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
+import nltk
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize
-
 # Write your solution here :
 def q01_longest_even_word(sentence):
-    
-    
+    words = word_tokenize(sentence)
+    longest_word = 0
+    for word in words:
+        if len(word)%2==0 and len(word)>len(str(longest_word)):
+            longest_word=word
+    return longest_word
+
+
