3b9bf27af2b34fc8bcfb58b2b5d25a6035863004
clydemendonca <clyde.mendonca01@gmail.com>
2018-08-14 04:55:13 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,3 +1,4 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
@@ -5,5 +6,18 @@ from nltk.tokenize import word_tokenize
 
 # Write your solution here :
 def q01_longest_even_word(sentence):
+#     tokens = word_tokenize(sentence)
+    tokens = (sentence.strip()).split(' ')
+    lenghts_of_tokens = [len(token) for token in tokens if len(token) % 2 == 0]
+    if len(lenghts_of_tokens) > 0:
+        max_length = max(lenghts_of_tokens)
+        for token in tokens:
+            if len(token) == max_length:
+                return token
+    return 0
     
-    
+
+
+
+
+
