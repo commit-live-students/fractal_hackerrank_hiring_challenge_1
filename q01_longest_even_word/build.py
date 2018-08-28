452cabb19fc3c4defe886fe3d19378696e9e07be
y0ge5h <c.9.yogesh@gmail.com>
2018-08-28 13:56:41 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,21 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
-
+from nltk.tokenize import word_tokenize
 
 # Write your solution here :
-def q01_longest_even_word(sentence):
-    
-    
+def q01_longest_even_word(words):
+    words = words.split(' ')
+    max_length = 0;''
+    result = ''
+    for x in words:
+        if len(x) % 2 == 0 and len(x) > max_length:
+            max_length = len(x)
+            result = x
+    if max_length <= 0:
+        return 0
+    else:
+        return result
+
+
