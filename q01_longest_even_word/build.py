7b8cac1711a34a6a7920438ed5d8eae83a2a170e
sonik8494 <sourabh.niks1994@gmail.com>
2018-08-31 08:40:23 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,30 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
-
+from nltk.tokenize import word_tokenize
 
 # Write your solution here :
+sentence = 'photographs or other images, readers can start to get a sense about the topic. This scanning and skimming helps set the expectation for the reading.'
+data1 = 'One great way to make predictions about an unfamiliar nonfiction text is to take a walk through the book before reading.'
+
 def q01_longest_even_word(sentence):
-    
-    
+    new = sentence.split(' ')
+    for x in new:
+        if x == '':
+            new.pop(0)
+    even = [x for x in new if len(x)%2 == 0]
+    if len(even) != 0:
+        return max(even,key = len)
+    if len(even) == 0:
+        return 0
+q01_longest_even_word(data1)
+# a = ' testing very9 important'
+# new = a.split(' ')
+# even = [x for x in new if len(x)%2 == 0]
+# for x in new:
+#     if x == '':
+#         new.pop(0)
+# new
+
+
