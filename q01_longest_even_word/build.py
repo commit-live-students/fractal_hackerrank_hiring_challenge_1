edaa74923046b27c386e9c75802009dc7ebe2fe2
vaibhav-cric <vaibhavrane.cric@gmail.com>
2018-09-29 05:34:19 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,8 @@
-# Default imports
-
-import re
-
-
-# Write your solution here :
 def q01_longest_even_word(sentence):
-    
-    
+    even_list = [i for i in sentence.split() if len(i)%2==0]
+    output = max(even_list, key=len) if even_list else 0
+    return output
+q01_longest_even_word('my name is bhavesh'
+
+
+
