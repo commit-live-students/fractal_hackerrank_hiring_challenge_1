9ac8bfe2b04b2fb03a34a77d4bcfcca901ca75ee
sunilhariharan <sunilhariharan@gmail.com>
2018-09-29 05:00:52 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,8 @@
-# Default imports
-
-import re
-
-
-# Write your solution here :
 def q01_longest_even_word(sentence):
-    
-    
+    even_list = [x for x in sentence.split() if len(x) % 2 == 0]
+    output = max(even_list, key=len) if even_list else 0
+    return output
+sentence='mya namea ias bhavesh'
+q01_longest_even_word(sentence)
+
+
