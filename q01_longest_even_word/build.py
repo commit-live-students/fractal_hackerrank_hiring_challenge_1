437965b430f7717d1343b28bee008a6cac0cd58d
PoojaTatkare <poojatatkare8@gmail.com>
2018-09-29 07:50:53 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,3 +1,4 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
@@ -5,5 +6,20 @@ import re
 
 # Write your solution here :
 def q01_longest_even_word(sentence):
+    a= sentence.split()
+    b= list(filter(lambda x:x if len(x)%2 == 0 else 0,a))
+    
+    if len(b)>0:
+        return max(b,key=len)
+    else:
+        return 0
+    
+    return b
+
     
     
+
+sentence= ''
+q01_longest_even_word(sentence)
+
+
