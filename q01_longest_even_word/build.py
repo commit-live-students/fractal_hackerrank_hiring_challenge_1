fef299430d70777f3679287ad58142ce2ec3f600
sannidh <soniknitr@gmail.com>
2018-09-29 04:58:25 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,15 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
 
-
+sentence='Time t cod eve numbe'
 # Write your solution here :
 def q01_longest_even_word(sentence):
-    
-    
+    s3=list(filter(lambda x:(len(x)%2==0), sentence.split()))
+    output=max(s3,key=len) if s3 else 0
+    return output
+
+
+
+
