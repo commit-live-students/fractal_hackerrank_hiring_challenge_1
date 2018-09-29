daf4732979b40fb7ae4cd1ebb1548a16e9c5519e
Sandesh373 <dyavanapellisandesh373@gmail.com>
2018-09-29 06:52:31 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,21 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
 
 
-# Write your solution here :
 def q01_longest_even_word(sentence):
+    x=[]
+    sentence=sentence.split()
+    for word in sentence:
+        if len(word)%2==0: 
+            x.append(word)
     
+    longest_word=max(x,key=lambda word:len(word)) if x else 0 
+    return longest_word
+          
     
+
+q01_longest_even_word('photographs or other images, readers can start to get a sense about the topic. This scanning and skimming helps set the expectation for the reading.')
+
+
