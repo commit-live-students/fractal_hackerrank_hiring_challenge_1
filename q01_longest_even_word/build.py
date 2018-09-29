3f574a539608194ba0f559c800d0bf8907bc6d76
ashit97 <ashitsoni97@gmail.com>
2018-09-29 07:35:00 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,9 +1,15 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
 
-
+a='my name is Bhavesh'
 # Write your solution here :
+mystr='my name is Bhavesh'
 def q01_longest_even_word(sentence):
+    even_list = [x for x in sentence.split() if len(x)%2==0]
+    output = max(even_list, key=len) if even_list else 0
+    return output
     
-    
+
+
