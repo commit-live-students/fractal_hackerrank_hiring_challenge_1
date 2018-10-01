cbe39cb08979b5f79c36ff5eff0510194bb139ea
Jas-simran <simranbright42@gmail.com>
2018-10-01 22:57:37 +0000

    Done

--- q01_longest_even_word/build.py
+++ q01_longest_even_word/build.py
@@ -1,3 +1,4 @@
+# %load q01_longest_even_word/build.py
 # Default imports
 
 import re
@@ -5,5 +6,26 @@ import re
 
 # Write your solution here :
 def q01_longest_even_word(sentence):
+    l1=sentence.split()
+    
+    for x in l1[:]:
+        if (len(x)%2!=0):
+            l1.remove(x)
+                        
+            
+    return max(l1,key=len,default=0)
+     
+            
+
+             
+ 
+        
     
     
+
+    
+
+q01_longest_even_word('My name is Simran')
+
+
+
